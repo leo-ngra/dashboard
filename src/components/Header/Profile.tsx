a329e38b5add4b2e47228012a2f298218e77d2cd
fakebmarchete <leonardo.ngra@gmail.com>
2022-09-14 10:30:12 -0300

    add responsividade

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,22 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
+import { RiTreasureMapFill } from "react-icons/ri";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
-        <Flex align='center'>
-            <Box mr='4' textAlign='right'>
+        <Flex align='center' >
+            
+            { showProfileData && (
+                <Box mr='4' textAlign='right'>
                 <Text>Leonardo Nogueira</Text>
                 <Text color='gray.300' fontSize='small'>
                     leonardo.ngra@gmail.com
                 </Text>
             </Box>
+            )}
 
             <Avatar size='md' name='Leonardo Nogueira' src='http://github.com/leo-ngra.png' />
         </Flex>
