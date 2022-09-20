c916c168efafadee6e25922293d149c8fe76ad91
fakebmarchete <leonardo.ngra@gmail.com>
2022-09-20 09:42:18 -0300

    logica paginacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -81,7 +81,7 @@ export default function UserList() {
                                 </Tbody>
                             </Table>
 
-                            <Pagination />
+                            <Pagination totalCountOfRegisters={200} currentPage={5} onPageChange={() => {}}/>
                         </>
                     )}
                 </Box>
