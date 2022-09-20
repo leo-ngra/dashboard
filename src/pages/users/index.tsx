66e350c35b923b3b0d2f19028103e41e08135425
fakebmarchete <leonardo.ngra@gmail.com>
2022-09-20 12:21:52 -0300

    logica da paginacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,11 +6,13 @@ import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useState } from "react";
 
 
 export default function UserList() {
 
-    const { data, isLoading, error } = useUsers()
+    const [page, setPage] = useState(1)
+    const { data, isLoading, error } = useUsers(page)
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -59,7 +61,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={['4', '4', '6']}>
@@ -81,7 +83,7 @@ export default function UserList() {
                                 </Tbody>
                             </Table>
 
-                            <Pagination totalCountOfRegisters={200} currentPage={5} onPageChange={() => {}}/>
+                            <Pagination totalCountOfRegisters={data.totalCount} currentPage={page} onPageChange={setPage}/>
                         </>
                     )}
                 </Box>
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -8,26 +8,40 @@ type User = {
     createdAt: string;
 }
 
-export async function getUsers(): Promise<User[]> {
-    const { data } = await api.get('users')
-    
-   const users = data.users.map(user => {
-    return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-        })
-     }
-   })
-   return users
+type GetUsersResponse = {
+    totalCount: number;
+    users: User[];
 }
 
-export function useUsers() {
-    return useQuery('users', getUsers, {
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+    const { data, headers } = await api.get('users', {
+        params: {
+            page,
+        }
+    })
+
+    const totalCount = Number(headers['x-total-count'])
+
+    const users = data.users.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+    return {
+        users,
+        totalCount
+    }
+}
+
+export function useUsers(page: number) {
+    return useQuery(['users', page], () => getUsers(page), {
         staleTime: 1000 * 5, // 5 seg
     })
 }
